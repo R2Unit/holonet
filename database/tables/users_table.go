8c40e61034ec775e789fb5f370bf8d71a0bf0b9c
r2unit <71521300+R2Unit@users.noreply.github.com>
2025-04-18 14:11:55 +0200

    Clean up users table migration by removing commented code
    Removed unused and commented-out columns to improve readability and maintainability of the users table migration file. This streamlines the code and ensures that only active and necessary fields are defined.

--- database/tables/users_table.go
+++ database/tables/users_table.go
@@ -5,20 +5,14 @@ import "github.com/holonet/core/database"
 var usersTable = database.TableMigration{
 	Name: "users",
 	Columns: map[string]string{
-		"id":       "SERIAL PRIMARY KEY",
-		"username": "VARCHAR(255) NOT NULL UNIQUE",
-		//"firstname":     "VARCHAR(255)",
-		//"lastname":       "VARCHAR(255)",
+		"id":            "SERIAL PRIMARY KEY",
+		"username":      "VARCHAR(255) NOT NULL UNIQUE",
 		"email":         "VARCHAR(255) NOT NULL UNIQUE",
 		"password_hash": "VARCHAR(255) NOT NULL",
 		"last_login":    "TIMESTAMP",
-		//"last_ip":       "VARCHAR(255)",
-		//"group_id":   "INTEGER NOT NULL REFERENCES groups(id) ON DELETE CASCADE"
-		//
-		// <lorenzo> Divider for my eyes only 0_o
-		"created_at": "TIMESTAMP NOT NULL DEFAULT NOW()",
-		"updated_at": "TIMESTAMP NOT NULL DEFAULT NOW()",
-		"deleted_at": "TIMESTAMP  NOT NULL DEFAULT NOW()",
+		"created_at":    "TIMESTAMP NOT NULL DEFAULT NOW()",
+		"updated_at":    "TIMESTAMP NOT NULL DEFAULT NOW()",
+		"deleted_at":    "TIMESTAMP  NOT NULL DEFAULT NOW()",
 	},
 	Priority: 1,
 }
