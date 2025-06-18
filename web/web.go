0246bf88d0686c1fe840c81174b9569595fa4110
r2unit <r2unit@proton.me>
2025-06-18 18:26:01 +0200

    Remove outdated comment in StartServer function
    The comment in the StartServer function was redundant as the function name is self-explanatory. Its removal improves code clarity and eliminates unnecessary clutter.

--- web/web.go
+++ web/web.go
@@ -6,7 +6,6 @@ import (
 	"github.com/holonet/core/logger"
 )
 
-// StartServer initializes and starts the HTTP server on the specified address
 func StartServer(addr string) {
 	logger.Info("Starting web server on %s", addr)
 	err := http.ListenAndServe(addr, nil)
