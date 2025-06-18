e9416c07a0ae5301e4de43de94b84b71d2d33c1b
r2unit <r2unit@proton.me>
2025-06-18 18:23:49 +0200

    Remove StartServer function from API module
    The StartServer function was unnecessary in this context and has been removed to streamline the API module. Any server-level setup should now be handled outside of this package for better separation of concerns.

--- api/api.go
+++ api/api.go
@@ -1,9 +1,6 @@
 package api
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/holonet/core/database"
 )
 
@@ -11,11 +8,4 @@ var dbHandler *database.DBHandler
 
 func SetDBHandler(handler *database.DBHandler) {
 	dbHandler = handler
-}
-
-func StartServer(addr string) {
-	log.Printf("Starting API service on %s", addr)
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Fatalf("Error starting API service: %v", err)
-	}
 }
